926d3766c71a7c1c18c54c8bcbed3409c141937b
sridevi-naveen <98551427+sridevi-naveen@users.noreply.github.com>
2022-03-11 15:49:56 +0000

    Add files via upload

---
+++ Rock band name.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator")
+city = input("What's name of the city you grew up in?")
+friend = input("What is your friend's name?")
+pet = input("What's your pet's name?")
+print("Your band name could be " + " " + city + " " + "friend" + " " +pet)
+
+
+
