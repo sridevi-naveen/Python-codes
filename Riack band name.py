89eff7efcd466de5a500c0a0049d7b1be627ea66
sridevi-naveen <98551427+sridevi-naveen@users.noreply.github.com>
2022-03-11 11:37:56 +0000

    Add files via upload

---
+++ Riack band name.py
@@ -0,0 +1,16 @@
+print("Welcome to the Band Name Generator")
+print("What's name of the city you grew up in?")
+def your_name(yourname):
+    return yourname
+print("What's your pet's name?")
+
+def pet_name(petname):
+    
+    return petname
+
+print("Your band name could be " + yourname + petname)
+
+print(your_name("Sri"))
+print(pet_name("Rabbit"))
+
+
